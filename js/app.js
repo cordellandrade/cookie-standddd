f6ad73c7f717d5b384cd980397fec7c0684a561d
Cordell Andrade <cordellandrade93@gmail.com>
2020-10-19 23:20:50 -0500

    Started class-07

--- js/app.js
+++ js/app.js
@@ -2,19 +2,60 @@
 
 // Store Hours
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am,', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-var seattleUl = document.getElementById('seattle');
-var tokyoUl = document.getElementById('tokyo');
-var dubaiUl = document.getElementById('dubai');
-var parisUl = document.getElementById('paris');
-var limaUl = document.getElementById('lima');
+var cookieTable = document.getElementById('cookie-table');
+
 
 
 // Location Objects
+
+function Store(name, minCust, maxCust, aveSale) {
+    this.name = name;
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.aveSale = aveSale;
+    this.cookiesPurchasedPerHourArr = [];
+    this.dailyStoreTotal = 0;
+    this.getRanCust = function() {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+    };
+    this.cookiesPurchasedPerHour = function() {
+        for (var i = 0; i < hours.length; i++) {
+            var cookiesPerHour = Math.ceil(this.getRanCust() * this.aveSale);
+            this.cookiesPurchasedPerHourArr.push(cookiesPerHour);
+            this.dailyStoreTotal += cookiesPerHour;
+        }
+    }
+    this.render = function() {
+        this.cookiesPurchasedPerHour();
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        td.textContent = this.name;
+        tr.appendChild(td);
+
+        for (var i = 0; i < hours.length; i++) {
+            // Create an element
+            var td = document.createElement('td');
+            // Give it content
+            td.textContent = this.cookiesPurchasedPerHourArr[i];
+            // Append to the DOM
+            tr.appendChild(td);
+        }
+        var td = document.createElement('td');
+        td.textContent = this.dailyStoreTotal;
+        tr.appendChild(td);
+        cookieTable.appendChild(tr);
+    }
+}
+
+var seattle = new Store('Seattle', 23, 65, 6.3);
+var tokyo = new Store('Tokyo', 3, 24, 1.2);
+
+
+/* 
 var seattle = {
     minCust: 23,
     maxCust: 65,
     aveSale: 6.3,
-    cust: 0,
     cookiesPurchasedPerHourArr: [],
     dailyStoreTotal: 0,
 
@@ -204,10 +245,11 @@ var lima = {
     }
 };
 
+*/
+
 
 seattle.render();
 tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
-
